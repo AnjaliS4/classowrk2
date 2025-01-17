4ec1b35aa1056eda1df240b5b74253c6351650a3
Anjali <anjalisimkhada5@gmail.com>
2025-01-17 16:08:20 +0545

    question 5 of the assignment

--- question1.py
+++ question1.py
@@ -1,9 +1,9 @@
 #Deque  
 # first question: Task manager with Deque 
 from collections import deque
-import random 
 
-#Initialize deck of cards 
+
+ 
 task_manager = deque(f"{value}" for value in
 ["Task: clean the room", "task 2 : Finish Uni assignment", "task3: finish reading The Alchemist"])
 #tasks =  (["Task 1: clean the room", "Task 2: finish uni assignment", "Task3: Finish reading Alchemist"])
@@ -20,15 +20,17 @@ print (task_manager)
 
 print("Processing tasks:")
 
+# remove task
+
+task_manager.popleft()
+print(f"first task removed: {task_manager}")
+
 # rotate task 
 task_manager.rotate(4)
 
 print (task_manager)
  
-# remove task
 
-task_manager.popleft()
-print(f"first task removed: {task_manager}")
 
 if len(task_manager) == 0: 
     Print("There are no tasks to perform ")
---
+++ question5.py
@@ -0,0 +1,8 @@
+import numpy as np 
+# create a 2D NumpY array 
+arr1 = np.array([[1, 2, 3], [4, 5, 6]])
+arr2 = np.array([[5, 10], [15, 20], [2, 4]])
+
+# now to multiply the 2 D arrays 
+array_multiplied = np.dot(arr1, arr2)
+print("the matric multiplication of the two 2D arrays are:", array_multiplied)
